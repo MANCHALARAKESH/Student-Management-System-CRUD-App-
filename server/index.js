6191fd8201fa744b70ac7d7d1d5e41a3d42c78f4
Ajay Kumar <gaddamajaykumar1@gmail.com>
2026-03-02 16:10:23 +0530

    Resolved merge conflict between main and database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(express.json());
@@ -8,17 +9,13 @@ app.get("/", (req, res) => {
   res.send("Backend Running 🚀");
 });
 
-
 // Connect to MongoDB
 mongoose.connect("mongodb://localhost:27017/student-management-system", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.error("Error connecting to MongoDB:", err));    
-
-
-
+.catch((err) => console.error("Error connecting to MongoDB:", err));
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
