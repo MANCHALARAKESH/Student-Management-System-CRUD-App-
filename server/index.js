d3e0b888ca4f748d4b2ce5cad8bb6190da84f0f8
Ajay Kumar <gaddamajaykumar1@gmail.com>
2026-03-02 16:23:16 +0530

    Resolved upstream merge conflict

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running 🚀");
+});
+
+// Connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/student-management-system", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Error connecting to MongoDB:", err));
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
