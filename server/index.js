ccc7f45ac85750c5c77f04ecffa5423cd830f842
Ajay Kumar <gaddamajaykumar1@gmail.com>
2026-03-02 17:24:16 +0530

    files created database setup success

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.get("/", (req, res) => {
 });
 
 mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch((err) => console.error("❌ Connection error:", err));
 
---
+++ server/routes/AuthRoutes.js
@@ -0,0 +1,4 @@
+const express=require('express');
+const user_data=require('../model/user');
+const router=express.Router();
+
